28bac76bf3b7693ebc3bd79a0fd9a90a18cd57c4
mysicol <ah3658@drexel.edu>
2023-03-11 15:05:58 -0500

    Added image generation through DALLE

---
+++ dalle.js
@@ -0,0 +1,22 @@
+async function getDalleResponse(inpPrompt) {
+  const { Configuration, OpenAIApi } = require("openai");
+  const configuration = new Configuration({
+    apiKey: "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",
+  });
+  const openai = new OpenAIApi(configuration);
+  try
+  {
+    const response = await openai.createImage({
+      prompt: inpPrompt,
+      n: 1,
+      size: "512x512",
+    });
+
+    console.log(response.data.data[0].url)
+    return response.data.data[0].url
+  }
+  catch(error)
+  {
+    throw(error);
+  }
+}
