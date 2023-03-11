1fceef91e02a694ee4d72df82e1789b4089887c6
mysicol <ah3658@drexel.edu>
2023-03-11 15:34:16 -0500

    Added DallE class

--- dalle.js
+++ dalle.js
@@ -1,22 +1,25 @@
-async function getDalleResponse(inpPrompt) {
-  const { Configuration, OpenAIApi } = require("openai");
-  const configuration = new Configuration({
-    apiKey: "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",
-  });
-  const openai = new OpenAIApi(configuration);
-  try
-  {
-    const response = await openai.createImage({
-      prompt: inpPrompt,
-      n: 1,
-      size: "512x512",
+class DallE {
+  constructor() {}
+  async getDalleResponse(inpPrompt) {
+    const { Configuration, OpenAIApi } = require("openai");
+    const configuration = new Configuration({
+      apiKey: "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",
     });
+    const openai = new OpenAIApi(configuration);
+    try
+    {
+      const response = await openai.createImage({
+        prompt: inpPrompt,
+        n: 1,
+        size: "512x512",
+      });
 
-    console.log(response.data.data[0].url)
-    return response.data.data[0].url
-  }
-  catch(error)
-  {
-    throw(error);
+      console.log(response.data.data[0].url)
+      return response.data.data[0].url
+    }
+    catch(error)
+    {
+      throw(error);
+    }
   }
 }
