45b7e180a96d7c06733c29f50045cb7bb4328878
Charbot <wdman2703@gmail.com>
2023-03-12 10:51:34 -0400

    Updating key again

--- chatgpt.js
+++ chatgpt.js
@@ -10,7 +10,7 @@ class ChatGPT {
         xhr.open("POST", url);
 
         xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("Authorization", "Bearer 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");
+        xhr.setRequestHeader("Authorization", "Bearer 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");
 
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4) {
